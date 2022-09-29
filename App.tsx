a127e3dac48c66972a279016523095abad06f1fd
Victor Canto <iamvictorcanto@gmail.com>
2022-09-29 20:27:57 -0300

    chore: installed prettier, eslint and setup

--- App.tsx
+++ App.tsx
@@ -1,31 +1,45 @@
-import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import React from "react";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import {
-  StyleSheet,
-  SafeAreaView,
-  Text,
-  View,
-  Platform,
-  StatusBar,
-} from 'react-native';
+    StyleSheet,
+    SafeAreaView,
+    Text,
+    View,
+    Platform,
+    StatusBar,
+} from "react-native";
 
-const isAndroid = Platform.OS === 'android';
+const isAndroid = Platform.OS === "android";
 
 export default function App() {
-  return (
-    <>
-      <SafeAreaView
-        style={{ flex: 1, marginTop: isAndroid ? StatusBar.currentHeight : 0 }}>
-        <View style={{ padding: 16, backgroundColor: 'green' }}>
-          <Text>Search</Text>
-        </View>
+    return (
+        <>
+            <SafeAreaView style={styles.container}>
+                <View style={styles.search}>
+                    <Text>Search</Text>
+                </View>
 
-        <View style={{ flex: 1, padding: 16, backgroundColor: 'blue' }}>
-          <Text>List</Text>
-        </View>
-      </SafeAreaView>
-      <ExpoStatusBar style='auto' />
-    </>
-  );
+                <View style={styles.list}>
+                    <Text>List</Text>
+                </View>
+            </SafeAreaView>
+            <ExpoStatusBar style="auto" />
+        </>
+    );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: isAndroid ? StatusBar.currentHeight : 0,
+    },
+    search: {
+        padding: 16,
+        backgroundColor: "green",
+    },
+    list: {
+        flex: 1,
+        padding: 16,
+        backgroundColor: "blue",
+    },
+});
