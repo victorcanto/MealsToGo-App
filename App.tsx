e29d96c5d51a3b2e06ea2f2c32a24e33a8cc823e
Victor Canto <iamvictorcanto@gmail.com>
2022-09-29 20:05:04 -0300

    feat: StatusBar spacing config

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import {
+  StyleSheet,
+  SafeAreaView,
+  Text,
+  View,
+  Platform,
+  StatusBar,
+} from 'react-native';
+
+const isAndroid = Platform.OS === 'android';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView
+        style={{ flex: 1, marginTop: isAndroid ? StatusBar.currentHeight : 0 }}>
+        <View style={{ padding: 16, backgroundColor: 'green' }}>
+          <Text>Search</Text>
+        </View>
+
+        <View style={{ flex: 1, padding: 16, backgroundColor: 'blue' }}>
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style='auto' />
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
