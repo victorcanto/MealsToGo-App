ff6195e67a4bb8b93417ec0a0faa76e0fee5920a
Victor Canto <iamvictorcanto@gmail.com>
2022-09-30 00:40:56 -0300

    feat: added custom fonts

--- App.tsx
+++ App.tsx
@@ -1,14 +1,34 @@
 import React from "react";
-import { Text } from "react-native";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { ThemeProvider, theme } from "@infra/theme";
 import { RestaurantScreen } from "@features/restaurants/screens/restaurants.screen";
 
+import {
+    useFonts as useOswaldFonts,
+    Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import {
+    useFonts as useLatoFonts,
+    Lato_400Regular,
+} from "@expo-google-fonts/lato";
+
 export default function App() {
+    const [oswaldLoaded] = useOswaldFonts({
+        Oswald_400Regular,
+    });
+
+    const [latoLoaded] = useLatoFonts({
+        Lato_400Regular,
+    });
+
+    if (!oswaldLoaded || !latoLoaded) {
+        return null;
+    }
+
     return (
         <>
             <ThemeProvider theme={theme}>
-               <Text>Screens...</Text>
+                <RestaurantScreen />
             </ThemeProvider>
 
             <ExpoStatusBar style="auto" />
